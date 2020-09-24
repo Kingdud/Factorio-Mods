83f43738b02361864e62ebe2eb6d4e2aed0f18d1
Kingdud <Kingdud5@gmail.com>
2020-09-24 20:49:00 +0100

    Fix power usage by AssemblerUPSGrade entities; they were not correctly factoring in beacon power drain.

--- AssemblerUPSGrade/prototypes/entity/entities.lua
+++ AssemblerUPSGrade/prototypes/entity/entities.lua
@@ -1,10 +1,14 @@
 require("prototypes.constants")
 require("prototypes.functions")
 
-function computePowerDraw(count, power_draw)
-	local total_beacon_pwr_drain = (count * math.ceil(beacon_count * .5) + beacon_count) * beacon_pwr_drain
+function ComputePowerDrainAndDraw(count, bld_power)
+	local num_beacons = count * math.ceil(beacon_count * 0.5) + beacon_count
+	local draw = bld_power * count
 	
-	return power_draw * count + total_beacon_pwr_drain
+	--Draw/30 is the standard drain for an entity with power where drain is not specified, per Factorio Wiki.
+	local drain = num_beacons * beacon_pwr_drain + draw / 30
+	
+	return draw, drain
 end
 
 function updateAssFluidBoxes(new_entity, assemblers, half_side_len)
@@ -47,6 +51,9 @@ function createAssemblerEntity(name, compression_ratio, n_ass, e_ass)
 	if DEBUG then
 		log("Debug: createAssemblerEntity " .. name)
 	end
+	
+	--modify here if you wish to create separate entities for normal/expensive mode.
+	local draw, drain = ComputePowerDrainAndDraw(n_ass, assembler_total_pwr_draw)
 		
 	--Pollution
 	--modify here if you wish to create separate entities for normal/expensive mode.
@@ -59,8 +66,8 @@ function createAssemblerEntity(name, compression_ratio, n_ass, e_ass)
 	new_entity.crafting_speed = 1
 	new_entity.crafting_categories = { name }
 	new_entity.energy_source.emissions_per_minute = total_pollution_value
-	--modify here if you wish to create separate entities for normal/expensive mode.
-	new_entity.energy_usage = computePowerDraw(n_ass, assembler_total_pwr_draw) .. "kW"
+	new_entity.energy_source.drain = drain .. "kW"
+	new_entity.energy_usage = draw .. "kW"
 	new_entity.allowed_effects = nil
 	new_entity.module_specification.module_slots = 0
 	new_entity.scale_entity_info_icon = true
@@ -121,6 +128,18 @@ function createAssemblerEntity(name, compression_ratio, n_ass, e_ass)
 		new_entity.fluid_boxes = nil
 	end
 	
+	--We want this before the scale factor gets reduced below.
+	local edge_art = {
+		filename = "__AssemblerUPSGrade__/graphics/entity/assembler-border.png",
+		frame_count = new_entity.animation.layers[1].frame_count,
+		line_length = new_entity.animation.layers[1].line_length,
+		height = 256,
+		priority = "high",
+		scale = scale_factor * .5,
+		width = 256,
+	}
+	table.insert(new_entity.animation.layers, edge_art)
+	
 	if DEBUG then
 		log("Debug createAssemblerEntity : " .. do_dump(new_entity))
 	end
@@ -134,13 +153,15 @@ function createChemPlantEntity(name, compression_ratio, n_chem, e_chem)
 	local total_pollution_value = chem_base_pollution * (chem_per_unit_pwr_drain_penalty * (prod_mod_pollution_penalty * chem_base_modules + 1)) * n_chem
 
 	local new_entity = util.table.deepcopy(base_chem_entity)
+	local draw, drain = ComputePowerDrainAndDraw(n_chem, chem_total_pwr_draw)
 	
 	new_entity.fixed_recipe = name .. "-recipe"
 	new_entity.name = name
 	new_entity.crafting_speed = 1
 	new_entity.crafting_categories = { name }
 	new_entity.energy_source.emissions_per_minute = total_pollution_value
-	new_entity.energy_usage = computePowerDraw(n_chem, chem_total_pwr_draw) .. "kW"
+	new_entity.energy_source.drain = drain .. "kW"
+	new_entity.energy_usage = draw .. "kW"
 	new_entity.allowed_effects = nil
 	new_entity.module_specification.module_slots = 0
 	new_entity.scale_entity_info_icon = true
@@ -168,7 +189,7 @@ function createChemPlantEntity(name, compression_ratio, n_chem, e_chem)
 	
 	--We want this before the scale factor gets reduced below.
 	local edge_art = {
-		filename = "__AssemblerUPSGrade__/graphics/entity/bld-border.png",
+		filename = "__AssemblerUPSGrade__/graphics/entity/chem-border.png",
 		frame_count = new_entity.animation["north"].layers[1].frame_count,
 		line_length = 6,
 		height = 256,
