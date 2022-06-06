3ac7dd833b7a4a47e9c3ab716c410a687183b672
Kingdud <Kingdud5@gmail.com>
2022-06-06 06:44:24 +0100

    OmniMatterMove now handles mixed fields just fine. I think. It had been crashing, but then it stopped? Not sure what I did to fix it.

--- KingsOmniMatterMove/control.lua
+++ KingsOmniMatterMove/control.lua
@@ -59,33 +59,33 @@ end
 --Mark field to move
 script.on_event(defines.events.on_player_selected_area, function(event)
 	if event.item == "ore-move-planner" then
+		global.ore_to_move[event.player_index] = nil
 		local player = game.players[event.player_index]
 		local surface = player.surface
 		local source_area = {
 			{event.area.left_top.x,	event.area.left_top.y},
 			{event.area.right_bottom.x,	event.area.right_bottom.y}}
 		local source_entities = surface.find_entities_filtered{area = source_area, type = "resource"}
-		local ent_count = 0
 		local ent_name = nil
+		if not global.ore_to_move[event.player_index] then
+			global.ore_to_move[event.player_index] = {}
+		end
 		for _, ent in pairs(source_entities) do
 			if ent.amount > 0 then
-				ent_count = ent_count + 1
-				if ent_name == nil then
-					ent_name = ent.name
-				elseif ent_name ~= ent.name then
-					game.print("[KingsOmnimatterMove]: cannot move multiple ore types at once.")
-					global.ore_to_move[event.player_index] = nil
-					return
+				if global.ore_to_move[event.player_index] and global.ore_to_move[event.player_index][ent.name] then
+					table.insert(global.ore_to_move[event.player_index][ent.name]["entities"], ent)
+					global.ore_to_move[event.player_index][ent.name]["ent_cnt"] = global.ore_to_move[event.player_index][ent.name]["ent_cnt"] + 1
+					global.ore_to_move[event.player_index][ent.name]["dest_square_side"] = math.ceil(math.sqrt(global.ore_to_move[event.player_index][ent.name]["ent_cnt"]))
+					--log("DEBUG existing: adding new ent " .. serpent.dump(ent, {compact = false, nocode = true, indent = "\t"}))
+				else
+					global.ore_to_move[event.player_index][ent.name] = { ["entities"] = {ent}, ["src_area"] = source_area, ["ent_cnt"] = 1, ["dest_square_side"] = math.ceil(math.sqrt(1)) }
+					--log("DEBUG new: adding new ent " .. serpent.dump(ent, {compact = false, nocode = true, indent = "\t"}))
 				end
 			end
 		end
-		--Always set the ore player table back to default to clean up old data when copying a new ore
-		global.ore_to_move[event.player_index] = {
-			entity_name = ent_name,
-			search_area = source_area,
-			entities = source_entities,
-			dest_square_side = math.ceil(math.sqrt(ent_count)),
-			enttity_count = ent_count}
+		if next(global.ore_to_move[event.player_index]) == nil then
+			global.ore_to_move[event.player_index]["chart_area"] = source_area
+		end
 	end
 end)
 
@@ -94,90 +94,112 @@ script.on_event(defines.events.on_player_alt_selected_area, function(event)
 	if event.item == "ore-move-planner" and global.ore_to_move[event.player_index] ~= nil then
 		local player = game.players[event.player_index]
 		local surface = player.surface
-		local source_area = global.ore_to_move[event.player_index].search_area
-		local source_entities = global.ore_to_move[event.player_index].entities
 		
-		if global.ore_to_move[event.player_index].entity_name == "crude-oil" then
-			local num_patches = global.ore_to_move[event.player_index].enttity_count
-			local right_x = get_oil_coordinates(num_patches-1)[1]
-			local top_y = get_oil_coordinates(OIL_MAX_PER_ROW)[2]
-			local bottom_y = get_oil_coordinates(OIL_MAX_PER_ROW-1)[2]
-			if num_patches < 20 then
-				top_y = get_oil_coordinates(0)[2]
-				bottom_y = get_oil_coordinates(num_patches-1)[2]
-			end
-			local dest_area = {
-				{event.area.left_top.x,	event.area.left_top.y + top_y},
-				{event.area.left_top.x + right_x,	event.area.left_top.y + bottom_y - 1}}
-			-- log(string.format("Dest_box x1 %d y1 %d x2 %d y2 %d", dest_area[1][1], dest_area[1][2], dest_area[2][1], dest_area[2][2]))
-			-- log(string.format("select_b x1 %d y1 %d x2 %d y2 %d", event.area.left_top.x, event.area.left_top.y, event.area.right_bottom.x, event.area.right_bottom.y))
-			-- log(string.format("offsets  x1 %d y1 %d x2 %d y2 %d", 0, top_y, right_x, bottom_y))
-			
-			--Note: the areas defined are different shapes compared to non-ore.
-			local blocker_tile = surface.find_tiles_filtered{area = dest_area, collision_mask = {"water-tile"}}
-			local blocker_ent = surface.find_entities_filtered{area = dest_area, type= {"cliff","resource"}}
-			
-			if next(blocker_ent) or next(blocker_tile) then
-				local ent = blocker_ent[next(blocker_ent)] or blocker_tile[next(blocker_tile)]
-				game.print("[KingsOmnimatterMove]: "..ent.name.." is in the way.")
-			else
-				local ent_count = 0
-				--If the surface has no blocking entities / tiles , continue
-				for _, ent in pairs(source_entities) do
-					local pos = get_oil_coordinates(ent_count)
-					
-					--Reminder, LUA arrays count from 1, because LUA is a retarded language.
-					pos[1] = pos[1] + event.area.left_top.x 
-					--Extra y spacing ensures we have room for pipes.
-					pos[2] = pos[2] + event.area.left_top.y
-								
-					if ent.amount > 0 then
-						surface.create_entity({name = ent.name , position = pos, force = ent.force, amount = ent.amount})
-						ent_count = ent_count + 1
-					end
-					ent.destroy()
+		if global.ore_to_move[event.player_index]["chart_area"] then
+			player.force.chart(player.surface, global.ore_to_move[event.player_index]["chart_area"])
+			global.ore_to_move[event.player_index] = nil
+			return
+		end
+		
+		local starting_x_pad = 0
+		local starting_y_pad = 0
+		for ent_name, ent_table in pairs(global.ore_to_move[event.player_index]) do
+			--log("DEBUG: " .. ent_name .. " || " .. ent_table.ent_cnt)
+			local source_area = ent_table.src_area
+			local source_entities = ent_table.entities
+		
+			if ent_name == "crude-oil" then
+				local num_patches = ent_table.ent_cnt
+				local right_x = get_oil_coordinates(num_patches-1)[1]
+				local top_y = get_oil_coordinates(OIL_MAX_PER_ROW)[2]
+				local bottom_y = get_oil_coordinates(OIL_MAX_PER_ROW-1)[2]
+				if num_patches < 20 then
+					top_y = get_oil_coordinates(0)[2]
+					bottom_y = get_oil_coordinates(num_patches-1)[2]
 				end
-				--Cause a chart to fire on the source_area and dest_area so the ore patch is visibly moved on the map.
-				player.force.chart(player.surface, source_area)
-				player.force.chart(player.surface, dest_area)
-				global.ore_to_move[event.player_index] = nil
-			end
-		else
-			local dest_square_side = global.ore_to_move[event.player_index].dest_square_side
-			local dest_area = {
-				{event.area.left_top.x,	event.area.left_top.y},
-				{event.area.left_top.x + dest_square_side - 1,	event.area.left_top.y + dest_square_side - 1}}
+				local dest_area = {
+					{event.area.left_top.x + starting_x_pad,			event.area.left_top.y + starting_y_pad + top_y},
+					{event.area.left_top.x + starting_x_pad + right_x,	event.area.left_top.y + starting_y_pad + bottom_y - 1}}
+				-- log(string.format("Dest_box x1 %d y1 %d x2 %d y2 %d", dest_area[1][1], dest_area[1][2], dest_area[2][1], dest_area[2][2]))
+				-- log(string.format("select_b x1 %d y1 %d x2 %d y2 %d", event.area.left_top.x, event.area.left_top.y, event.area.right_bottom.x, event.area.right_bottom.y))
+				-- log(string.format("offsets  x1 %d y1 %d x2 %d y2 %d", 0, top_y, right_x, bottom_y))
+				
+				--Note: the areas defined are different shapes compared to non-ore.
+				local blocker_tile = surface.find_tiles_filtered{area = dest_area, collision_mask = {"water-tile"}}
+				local blocker_ent = surface.find_entities_filtered{area = dest_area, type= {"cliff","resource"}}
+				
+				if next(blocker_ent) or next(blocker_tile) then
+					local ent = blocker_ent[next(blocker_ent)] or blocker_tile[next(blocker_tile)]
+					game.print("[KingsOmnimatterMove]: "..ent.name.." is in the way.")
+				else
+					local ent_count = 0
+					--If the surface has no blocking entities / tiles , continue
+					for _, ent in pairs(source_entities) do
+						local pos = get_oil_coordinates(ent_count)
+						
+						--Reminder, LUA arrays count from 1, because LUA is a retarded language.
+						pos[1] = pos[1] + event.area.left_top.x + starting_x_pad
+						--Extra y spacing ensures we have room for pipes.
+						pos[2] = pos[2] + event.area.left_top.y + starting_y_pad
+									
+						if ent.amount > 0 then
+							surface.create_entity({name = ent.name , position = pos, force = ent.force, amount = ent.amount})
+							ent_count = ent_count + 1
+						end
+						--log("DEBUG: destroying_curde " .. serpent.dump(ent, {compact = false, nocode = true, indent = "\t"}))
+						ent.destroy()
+					end
+					--Cause a chart to fire on the source_area and dest_area so the ore patch is visibly moved on the map.
+					player.force.chart(player.surface, source_area)
+					player.force.chart(player.surface, dest_area)
+					global.ore_to_move[event.player_index][ent_name] = nil
+				end
+				
+				--Now that we have used the pads, increment them to be to the right of this field, plus a spacer.
+				starting_x_pad = starting_x_pad + right_x + 5
+				--we don't increment starting y pad, maybe one day.
+				--starting_y_pad = starting_y_pad + bottom_y + 5
+			else
+				local dest_square_side = ent_table.dest_square_side
+				local dest_area = {
+					{event.area.left_top.x + starting_x_pad,						event.area.left_top.y + starting_y_pad},
+					{event.area.left_top.x + dest_square_side + starting_x_pad - 1,	event.area.left_top.y + dest_square_side + starting_y_pad - 1}}
 
-			--Algorithm side-effect: ore fields will be square. Side-effect acceptable.
-			local blocker_tile = surface.find_tiles_filtered{area = dest_area, collision_mask = {"water-tile"}}
-			local blocker_ent = surface.find_entities_filtered{area = dest_area, type= {"cliff","resource"}}
-			
-			if next(blocker_ent) or next(blocker_tile) then
-				local ent = blocker_ent[next(blocker_ent)] or blocker_tile[next(blocker_tile)]
-				game.print("[KingsOmnimatterMove]: "..ent.name.." is in the way.")
-			else
-				local ent_count = 0
-				--If the surface has no blocking entities / tiles , continue
-				for _, ent in pairs(source_entities) do
-					local pos = get_coordinates(ent_count, dest_square_side)
-					
-					--Reminder, LUA arrays count from 1, because LUA is a retarded language.
-					pos[1] = pos[1] + event.area.left_top.x 
-					--Extra y spacing ensures we have room for pipes.
-					pos[2] = pos[2] + event.area.left_top.y
-								
-					if ent.amount > 0 then
-						surface.create_entity({name = ent.name , position = pos, force = ent.force, amount = ent.amount})
-						ent_count = ent_count + 1
+				--Algorithm side-effect: ore fields will be square. Side-effect acceptable.
+				local blocker_tile = surface.find_tiles_filtered{area = dest_area, collision_mask = {"water-tile"}}
+				local blocker_ent = surface.find_entities_filtered{area = dest_area, type= {"cliff","resource"}}
+				
+				if next(blocker_ent) or next(blocker_tile) then
+					local ent = blocker_ent[next(blocker_ent)] or blocker_tile[next(blocker_tile)]
+					game.print("[KingsOmnimatterMove]: "..ent.name.." is in the way.")
+				else
+					local ent_count = 0
+					--If the surface has no blocking entities / tiles , continue
+					for _, ent in pairs(source_entities) do
+						local pos = get_coordinates(ent_count, dest_square_side)
+						
+						--Reminder, LUA arrays count from 1, because LUA is a retarded language.
+						pos[1] = pos[1] + event.area.left_top.x + starting_x_pad
+						--Extra y spacing ensures we have room for pipes.
+						pos[2] = pos[2] + event.area.left_top.y + starting_y_pad
+									
+						if ent.amount > 0 then
+							surface.create_entity({name = ent.name , position = pos, force = ent.force, amount = ent.amount})
+							ent_count = ent_count + 1
+						end
+						--log("DEBUG: destroying " .. serpent.dump(ent, {compact = false, nocode = true, indent = "\t"}))
+						ent.destroy()
 					end
-					ent.destroy()
+					--Cause a chart to fire on the source_area and dest_area so the ore patch is visibly moved on the map.
+					player.force.chart(player.surface, source_area)
+					player.force.chart(player.surface, dest_area)
+					global.ore_to_move[event.player_index][ent_name] = nil
 				end
-				--Cause a chart to fire on the source_area and dest_area so the ore patch is visibly moved on the map.
-				player.force.chart(player.surface, source_area)
-				player.force.chart(player.surface, dest_area)
-				global.ore_to_move[event.player_index] = nil
+				
+				starting_x_pad = starting_x_pad + dest_square_side + 5
 			end
 		end
+		global.ore_to_move[event.player_index] = nil
 	end
 end)
 
